63ad951a930cddf1d5551cc25bf5061194b95f68
FgalUR <galetskifedor@gmail.com>
2024-12-08 00:25:38 +0300

    adding smt in main

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Integer> integers=new ArrayList<>();
+        for(int i=0; i<20; i++){
+            integers.add(i);
+            System.out.println(integers);
+        }
+        int sum=0;
+        for(int i: integers){
+            sum+=i;
+        }
+        System.out.println("This is sum");
+    }
 }
